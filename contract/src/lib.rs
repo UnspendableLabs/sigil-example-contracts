3769b32f75737af64ba94ff47695a5c6efa1b4ff
wilfreddenton <wilfred@hey.com>
2025-09-08 15:20:55 -0400

    deps

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -5,7 +5,7 @@ contract!(name = "hello-world");
 impl Guest for HelloWorld {
     fn init(_ctx: &ProcContext) {}
 
-    fn hello_world(_: &ViewContext) -> String {
+    fn hello_world(_ctx: &ViewContext) -> String {
         "Hello, World!".to_string()
     }
 }
